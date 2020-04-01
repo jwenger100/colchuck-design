13eb36f7ad94b8700559de3f456b3c4b333389f5
Jonathan Wenger <jowenger@microsoft.com>
2020-04-01 10:42:38 -0700

    Updates

---
+++ precache-manifest.f5a335e760622aed2599858cfe5310ed.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29dcd2081edb83126828af954b1ef6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05fb6559af1a5c624a06",
+    "url": "/static/css/main.5df7d988.chunk.css"
+  },
+  {
+    "revision": "40410eb4168216c5b38e",
+    "url": "/static/js/2.298d2f1c.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.298d2f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fb6559af1a5c624a06",
+    "url": "/static/js/main.cf6e6476.chunk.js"
+  },
+  {
+    "revision": "afcb2679fec324f6bb5d",
+    "url": "/static/js/runtime-main.8ce6def8.js"
+  },
+  {
+    "revision": "4bba4fca7620bb4e13ff2cccb4f0058d",
+    "url": "/static/media/productBuoy.4bba4fca.svg"
+  },
+  {
+    "revision": "3a1df6c1e0644b8b1a12219346c3f013",
+    "url": "/static/media/productCurvyLines.3a1df6c1.png"
+  },
+  {
+    "revision": "a0b2ccce334edbf4107e46c0aef94b4a",
+    "url": "/static/media/productHowItWorks1.a0b2ccce.svg"
+  },
+  {
+    "revision": "138c5412d63f4ec5feac4282df15ba82",
+    "url": "/static/media/productHowItWorks2.138c5412.svg"
+  },
+  {
+    "revision": "8ca21716149eb07cd0557215642c47ce",
+    "url": "/static/media/productHowItWorks3.8ca21716.svg"
+  },
+  {
+    "revision": "5317c43c71e1f6199e6c0b297368091f",
+    "url": "/static/media/productValues1.5317c43c.svg"
+  },
+  {
+    "revision": "b8e7bdd2c72a7e5b0ce21b006e74f580",
+    "url": "/static/media/productValues2.b8e7bdd2.svg"
+  },
+  {
+    "revision": "40b631264f43f8c71f9a7801da84890b",
+    "url": "/static/media/productValues3.40b63126.svg"
+  }
+]);
